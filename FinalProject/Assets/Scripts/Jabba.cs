69d6c6a50c2e5a163c0bafe42b7168447adeb8ad
William McDermott <williamy.mcdermott@gmail.com>
2023-05-09 18:34:26 -0700

    f

--- FinalProject/Assets/Scripts/Jabba.cs
+++ FinalProject/Assets/Scripts/Jabba.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI; 
 public class Jabba : MonoBehaviour
 {
-    
+    public Text Score; // Reference to the UI text component that displays the counter value
+    private int counter = 0; // The counter variable that keeps track of the number of points
+
+    // This method is called when the button is clicked
+    public void AddPoint()
+    {
+        counter++; // Increment the counter
+        Score.text = "Points: " + counter; // Update the text component to display the new counter value
+    }
 }
+
+
